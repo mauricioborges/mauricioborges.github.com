893f33d69f3c2ce951cb170d35913430654a4a46
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2019-09-22 14:22:18 +0800

    cleans up

--- build.sh
+++ build.sh
@@ -1,10 +1,1 @@
 pandoc index.md > index.html
-
-rm -rf _posts
-
-mkdir _posts
-
-for post in $(ls posts);do
-  html_file=$(echo $post|sed "s/.md/.html/g")
-  pandoc posts/$post > _posts/$html_file
-done
--- posts/2019-9-22-defining_test_strategy.md
+++
@@ -1,5 +0,0 @@
-# 2019-9-22: an approach on defining a testing strategy when entering a new project
-
-1. Diagnosis
-2. Definition
-3. Action plan
